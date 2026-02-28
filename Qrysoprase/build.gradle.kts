f79888dcdb0f0654ffe8560a3bd9b5ca5806ca2b
wcaokaze <wcaokaze@gmail.com>
2026-03-01 00:07:58 +0900

    GenerateEventArgUtilsをbuildSrcに移動

--- Qrysoprase/build.gradle.kts
+++ Qrysoprase/build.gradle.kts
@@ -21,8 +21,6 @@ plugins {
    alias(libs.plugins.compose.compiler)
 }
 
-val generatedSrcDir = layout.buildDirectory.file("generated/src")
-
 kotlin {
    androidTarget()
    jvm("desktop")
@@ -36,10 +34,6 @@ kotlin {
    }
 
    sourceSets {
-      commonMain {
-         kotlin.srcDir(generatedSrcDir)
-      }
-
       commonMain.dependencies {
          implementation(compose.runtime)
       }
@@ -71,133 +65,13 @@ android {
    compileSdk = 35
 }
 
-open class GenerateEventArgUtils : DefaultTask() {
-   @get:OutputDirectory
-   val generatedSrcDir = project.layout.buildDirectory.file("generated/src").get().asFile
-
-   @TaskAction
-   fun generateUtils() {
-      val packageName = "com.wcaokaze.probosqis.qrysoprase"
-      val packageDir = File(
-         generatedSrcDir, packageName.replace(".", File.separator)
-      )
-
-      if (!packageDir.exists() && !packageDir.mkdirs()) {
-         throw GradleException("can not generate source dir: $packageDir")
-      }
-
-      operator fun IntRange.invoke(separator: String = ", ", mapper: (Int) -> String)
-          = joinToString(separator) { mapper(it) }
-
-      File(packageDir, "AddContextualEventArgs.kt").writer().use { writer ->
-         writer.write(
-            """
-               package $packageName
-
-               private inline fun <C : Function<Event>> Dispatcher(
-                  crossinline dispatcher: (eventConstructor: C) -> Unit
-               ): Dispatcher<C> {
-                  return object : Dispatcher<C>() {
-                     override fun invoke(eventConstructor: C) {
-                        dispatcher(eventConstructor)
-                     }
-                  }
-               }
-            """
-         )
-
-         for (p in 1..20) {
-            for (c in p..20) {
-               writer.write(
-                  """
-                     @JvmName("addContextualEventArgs${p - 1}To$c")
-                     fun <E : Event, ${(1..c) { "C$it" }}> Dispatcher<(${(1..<p) { "C$it" }}) -> E>.addContextualEventArgs(
-                        ${(p..c) { "context$it: C$it" }}
-                     ): Dispatcher<(${(1..c) { "C$it" }}) -> E> {
-                        return Dispatcher { eventConstructor ->
-                           val partialAppliedConstructor = { ${(1..<p) { "context$it: C$it" }} -> eventConstructor(${(1..c) { "context$it" }}) }
-                           this(partialAppliedConstructor)
-                        }
-                     }
-                  """
-               )
-            }
-         }
-      }
-
-      File(packageDir, "DispatchWithArgs.kt").writer().use { writer ->
-         writer.write("package $packageName")
-
-         for (c in 1..20) {
-            for (a in c..20) {
-               writer.write(
-                  """
-                     operator fun <E : Event, ${(1..<c)("") { "C$it, " }}${(c..a) { "A$it" }}>
-                        Dispatcher<(${(1..<c) { "C$it" }}) -> E>.invoke(
-                           eventConstructor: (${(1..<c)("") { "C$it, " }}${(c..a) { "A$it" }}) -> E,
-                           ${(c..a) { "arg$it: A$it" }}
-                        )
-                     {
-                        val constructor = fun (${(1..<c) { "context$it: C$it" }}): E {
-                           return eventConstructor(${(1..<c)("") { "context$it, " }}${(c..a) { "arg$it" }})
-                        }
-
-                        invoke(constructor)
-                     }
-                  """
-               )
-            }
-         }
-      }
-
-      File(packageDir, "ContextualEventArgs.kt").writer().use { writer ->
-         writer.write("package $packageName")
-
-         for (p in 1..20) {
-            for (c in p..20) {
-               writer.write(
-                  """
-                     @JvmName("contextualEventArgs${p - 1}To$c")
-                     context(parent: Dispatcher<(${(1..<p) { "C$it" }}) -> E>)
-                     inline fun <E : Event, R, ${(1..c) { "C$it" }}> contextualEventArgs(
-                        ${(p..c) { "context$it: C$it" }},
-                        block: context(Dispatcher<(${(1..c) { "C$it" }}) -> E>) () -> R
-                     ): R {
-                        return context(parent.addContextualEventArgs(${(p..c) { "context$it" }})) {
-                           block()
-                        }
-                     }
-                  """
-               )
-            }
-         }
-      }
-
-      File(packageDir, "ContextualEventArgsExtension.kt").writer().use { writer ->
-         writer.write("package $packageName")
-
-         for (p in 1..20) {
-            for (c in p..20) {
-               writer.write(
-                  """
-                     @JvmName("contextualEventArgsExtension${p - 1}To$c")
-                     inline fun <E : Event, R, ${(1..c) { "C$it" }}> Dispatcher<(${(1..<p) { "C$it" }}) -> E>.contextualEventArgs(
-                        ${(p..c) { "context$it: C$it" }},
-                        block: context(Dispatcher<(${(1..c) { "C$it" }}) -> E>) () -> R
-                     ): R {
-                        return context(addContextualEventArgs(${(p..c) { "context$it" }})) {
-                           block()
-                        }
-                     }
-                  """
-               )
-            }
-         }
-      }
+tasks.register<
+    com.wcaokaze.probosqis.qrysoprase.buildscript.GenerateEventArgUtils
+>("generateEventArgUtils") {
+   kotlin.sourceSets.commonMain {
+      kotlin.srcDir(outputDir)
    }
 }
-
-tasks.register<GenerateEventArgUtils>("generateEventArgUtils")
 
 tasks.configureEach {
    if (name.contains("compile") && name.contains("Kotlin")) {
